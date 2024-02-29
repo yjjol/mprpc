7c62a5d1a90873da22b84dbe79fcb98b8aa979fc
yjjol <2016720467@qq.com>
2024-02-29 15:41:32 +0800

    first commit code!

---
+++ build/src/CMakeFiles/mprpc.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/MPRPC/mprpc/src/logger.cc" "/home/MPRPC/mprpc/build/src/CMakeFiles/mprpc.dir/logger.cc.o"
+  "/home/MPRPC/mprpc/src/mprpcapplication.cc" "/home/MPRPC/mprpc/build/src/CMakeFiles/mprpc.dir/mprpcapplication.cc.o"
+  "/home/MPRPC/mprpc/src/mprpcchannel.cc" "/home/MPRPC/mprpc/build/src/CMakeFiles/mprpc.dir/mprpcchannel.cc.o"
+  "/home/MPRPC/mprpc/src/mprpcconfig.cc" "/home/MPRPC/mprpc/build/src/CMakeFiles/mprpc.dir/mprpcconfig.cc.o"
+  "/home/MPRPC/mprpc/src/mprpccontroller.cc" "/home/MPRPC/mprpc/build/src/CMakeFiles/mprpc.dir/mprpccontroller.cc.o"
+  "/home/MPRPC/mprpc/src/rpcheader.pb.cc" "/home/MPRPC/mprpc/build/src/CMakeFiles/mprpc.dir/rpcheader.pb.cc.o"
+  "/home/MPRPC/mprpc/src/rpcprovider.cc" "/home/MPRPC/mprpc/build/src/CMakeFiles/mprpc.dir/rpcprovider.cc.o"
+  "/home/MPRPC/mprpc/src/zookeeperutil.cc" "/home/MPRPC/mprpc/build/src/CMakeFiles/mprpc.dir/zookeeperutil.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/include"
+  "../example"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
